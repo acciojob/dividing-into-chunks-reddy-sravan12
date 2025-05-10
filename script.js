03703a26c7fa32068b3ed5e76153fa6a4b3ccdb6
reddy-sravan12 <101791343+reddy-sravan12@users.noreply.github.com>
2025-05-10 23:57:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-    let left=0
+    let left=0 
     let sum=arr[left]
     let subArray=[]
     for(let i=1;i<arr.length;i++){
         sum+=arr[i]
-        if(sum>=n){
+        if(sum>n){
             subArray.push(arr.slice(left,i))
              left=i
             sum=arr[i]
